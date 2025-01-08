5285065c7ed8e043f5c3ac5f4dae2ed257b432b3
Shalin Nipuna <shalin@nordictravels.eu>
2025-01-08 11:45:39 +0200

    Initial Commit

---
+++ storage/framework/views/0cac764a60e592f7cfe4c6ba90bfef25e8d238aa.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>NUT HR - Hours Tracker</title>
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+    <meta content="TTM internal solution" name="description" />
+    <meta content="CodeXcore Technologies" name="author" />
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <link href="plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+
+    <link href="<?php echo e(asset('assets/css/bootstrap.min.css')); ?>" rel="stylesheet" type="text/css">
+    <link href="<?php echo e(asset('assets/css/metismenu.min.css')); ?>" rel="stylesheet" type="text/css">
+    <link href="<?php echo e(asset('assets/css/icons.css')); ?>" rel="stylesheet" type="text/css">
+
+    <link href="<?php echo e(asset('assets/css/style.css')); ?>" rel="stylesheet" type="text/css">
+
+
+
+    
+    <link href="<?php echo e(asset('plugins/datatables/dataTables.bootstrap4.min.css')); ?>" rel="stylesheet" type="text/css" />
+    <link href="<?php echo e(asset('plugins/datatables/buttons.bootstrap4.min.css')); ?>" rel="stylesheet" type="text/css" />
+
+    <link href="<?php echo e(asset('/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css')); ?>" rel="stylesheet"
+        type="text/css" />
+
+    <!-- Responsive datatable examples -->
+    <link href="<?php echo e(asset('plugins/datatables/responsive.bootstrap4.min.css')); ?>" rel="stylesheet" type="text/css" />
+    <script src="<?php echo e(asset('assets/js/jquery.min.js')); ?>"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="<?php echo e(asset('plugins/morris/morris.css')); ?>">
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <style>
+        :root {
+            --scrollbar-always-visible: true;
+        }
+        
+        * {
+            -webkit-overflow-scrolling: touch;
+        }
+        
+        body {
+            overflow-y: scroll;
+        }
+        </style>
+</head>
+
+<?php echo $__env->make('partials.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php echo $__env->make('partials.sidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+<?php echo $__env->yieldContent('content'); ?>
+
+<?php echo $__env->make('partials.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php /**PATH C:\Users\OMISTAJA\Desktop\Projects\-nuthr\resources\views/partials/main.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/bac9aa60a443f2ac55b5d377c29264f50da52321.php
@@ -0,0 +1,64 @@
+<body>
+
+    <!-- Begin page -->
+    <div id="wrapper">
+
+        <!-- Top Bar Start -->
+        <div class="topbar">
+
+            <!-- LOGO -->
+            <div class="topbar-left">
+                <a href="<?php echo e(route('dashboard')); ?>" class="logo">
+                    
+                    <h4>Dashboard</h4>
+                </a>
+            </div>
+
+
+
+            <nav class="navbar-custom">
+                <ul class="navbar-right list-inline float-right mb-0">
+
+                    <li class="dropdown notification-list list-inline-item"><?php echo e(Auth::user()->name); ?></li>
+
+                    <!-- full screen -->
+                    <li class="dropdown notification-list list-inline-item d-none d-md-inline-block">
+                        <a class="nav-link waves-effect" href="#" id="btn-fullscreen">
+                            <i class="fas fa-expand noti-icon"></i>
+                        </a>
+                    </li>
+
+                    <li class="dropdown notification-list list-inline-item">
+                        <div class="dropdown notification-list nav-pro-img">
+                            <a class="dropdown-toggle nav-link arrow-none waves-effect nav-user" data-toggle="dropdown"
+                                href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                                <img src="<?php echo e(asset('assets/images/users/user-1.jpg')); ?>" alt="user"
+                                    class="rounded-circle">
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated profile-dropdown">
+                                <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" style="display: none;">
+                                    <?php echo csrf_field(); ?>
+                                </form>
+                                <a class="dropdown-item text-danger" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    <i class="mdi mdi-power text-danger"></i> Logout
+                                </a>
+                            </div>
+                            
+                        </div>
+                    </li>
+
+                </ul>
+
+                <ul class="list-inline menu-left mb-0">
+                    <li class="float-left">
+                        <button class="button-menu-mobile open-left waves-effect">
+                            <i class="mdi mdi-menu"></i>
+                        </button>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </div>
+        <!-- Top Bar End -->
+<?php /**PATH C:\Users\OMISTAJA\Desktop\Projects\-nuthr\resources\views/partials/header.blade.php ENDPATH**/ ?>
